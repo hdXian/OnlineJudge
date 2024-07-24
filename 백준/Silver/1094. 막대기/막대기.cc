3e3981912b3f954bfa05c8fe5636b7b7babbb146
Hwang Jehyun <76947781+hdXian@users.noreply.github.com>
2024-07-24 18:06:18 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    int x;
+    std::cin >> x;
+
+    int count = 0;
+    while (x > 0) {
+        // std::cout << x << " / 2 =  " << x / 2 << std::endl;
+        count += x % 2;
+        x /= 2;
+    }
+
+    std::cout << count;
+
+}
