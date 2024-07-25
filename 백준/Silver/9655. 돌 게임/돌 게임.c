4fc6c035194710eec83b93b9d364b239aa7142c8
Hwang Jehyun <76947781+hdXian@users.noreply.github.com>
2024-07-25 16:37:43 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.c
+++ 백준/Silver/9655. 돌 게임/돌 게임.c
@@ -4,8 +4,8 @@ int main(int argc, char** argv) {
     int n;
     scanf("%d", &n);
 
-    if((n%2) == 0) puts("CY");
-    else puts("SK");
+    if(n%2) puts("SK");
+    else puts("CY");
 
     return 0;
 }
