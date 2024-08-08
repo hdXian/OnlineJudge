c730d6f6071aeb76c95eed76c5807d4612741dba
Hwang Jehyun <76947781+hdXian@users.noreply.github.com>
2024-08-08 21:02:16 +0900

    [Silver III] Title: 주유소, Time: 40 ms, Memory: 31120 KB, Score: 41 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+# 각 도시로 넘어갈 때마다의 최소 비용을 구해서 더하면 될듯.
+# 어차피 다음 도시로 가려면 최소한 지금 도시나 이전까지의 도시들에서 기름을 구해와야 함.
+
+distances = list(map(int, input().split()))
+oil_prices = list(map(int, input().split())) # 마지막 도시의 연료값은 필요없긴 함
+
+# print("n:", n)
+# print("distances:", distances)
+# print("oil_prices:", oil_prices)
+
+# print(distances[:2])
+
+total_price = 0
+# 각 도로를 지날 때마다
+# 1번째 도로의 기름 수급처: 1번 도시
+# n번째 도로의 기름 수급처: 1, 2, 3, ..., n번째 도시
+for i in range(n-1): # i는 인덱스
+    # print("i:", i)
+    distance = distances[i]
+    min_price = min(oil_prices[:(i+1)])
+    total_price += distance * min_price
+
+# print("total_price:", total_price)
+print(total_price)
