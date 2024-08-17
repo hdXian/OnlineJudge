4630bf7981eec7d89b2ebb86f9827e663cb33d59
hdXian <hyun353500@naver.com>
2024-08-18 01:13:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,29 +1,15 @@
-# 물건을 정확히 n 킬로그램 배달.
 n = int(input())
-# print("n:", n)
-
-# 가장 적은 봉지 수로 물건을 포장
-# 봉지는 5kg짜리, 3kg짜리가 있음.
 
 def check(num):
     count = -1
     max_5 = num // 5
-    # print("max_5:", max_5)
     for i in range(max_5, -1, -1):
-        # print("i:", i)
-        tmp_count = i # 5kg 봉지 수
-        rest = num - (5*i) # 3kg으로 처리해야 할 무게
-        # print("rest:", rest)
+        tmp_count = i
+        rest = num - (5*i)
         if rest%3 == 0:
             tmp_count += rest//3
             count = tmp_count
             return count
-    return count # 안되면 -1 리턴
+    return count
 
-
-# 3 <= n <= 5000
-# memo = [0, 0, 0, 1, -1, 1, 2, -1, 2, 3, 2, 3, 4, 3, 4, 3]
-
-result = check(n)
-# print("result:", result)
-print(result)
+print(check(n))
