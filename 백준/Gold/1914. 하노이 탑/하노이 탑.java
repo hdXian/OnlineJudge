f9e9070ab1ee7b7bbd5ce4b8530b97dc87dbda76
hdXian <hyun353500@naver.com>
2024-10-07 23:19:42 +0900

    [Gold V] Title: 하노이 탑, Time: 368 ms, Memory: 37324 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static StringBuilder builder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        BigInteger two = new BigInteger("2");
+
+        // 이동 횟수는 공식으로
+        BigInteger num = two.pow(n).subtract(BigInteger.valueOf(1));
+
+        builder.append(num.toString());
+        builder.append("\n");
+
+        // n이 20 이하면 과정도 출력
+        if (n<=20) {
+            hanoi(n, 1, 2, 3);
+        }
+
+        System.out.println(builder.toString());
+
+    }
+
+    private static void hanoi(int n, int src, int middle, int dst) throws IOException {
+        if (n==0)
+            return;
+
+        // 마지막을 제외한 n-1개의 원판을 출발에서 중간으로 이동 (마지막 장대를 경유로 사용)
+        hanoi(n-1, src, dst, middle);
+
+        // 마지막 원판을 출발에서 마지막으로 이동
+        builder.append(src);
+        builder.append(" ");
+        builder.append(dst);
+        builder.append("\n");
+
+        // 중간으로 이동한 n-1개의 원판을 중간에서 마지막으로 이동 (출발 장대를 경유로 사용)
+        hanoi(n-1, middle, src, dst);
+    }
+
+}
