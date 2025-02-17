6b0ab9518cfa44268b42b99637cccf8b43c9c1f3
hdXian <hyun353500@naver.com>
2025-02-18 03:09:57 +0900

    [Gold II] Title: 피자판매, Time: 160 ms, Memory: 20136 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.java
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -11,7 +11,8 @@ public class Main {
     public static int[] tableA;
     public static int[] tableB;
 
-    public static int[] pieces;
+    public static int[] piecesA;
+    public static int[] piecesB;
 
     public static void main(String[] args) throws Exception {
 
@@ -32,89 +33,29 @@ public class Main {
 
         // 피자 A 조각 입력받기
         // ex) 2 2 1 7 2
-        pieces = new int[M];
+        piecesA = new int[M];
         int piece;
         for(int i=0; i<M; i++) {
             piece = Integer.parseInt(reader.readLine());
-            pieces[i] = piece;
+            piecesA[i] = piece;
             if (piece <= P)
                 tableA[piece] += 1;
         }
 
         // 피자 개수별로 나올 수 있는 조합 계산
-
-        // 모든 피자 조각을 사용하는 경우 계산
-        int tmp = 0;
-        for(int k=0; k<M; k++) {
-            tmp += pieces[k];
-        }
-        if (tmp <= P)
-            tableA[tmp] = 1;
-
-        for(int i=2; i<M; i++) {
-            int pre_size = 0;
-
-            // 첫번째 조각부터 i개의 조각 이어붙이기
-            for(int k=0; k<i; k++) {
-                pre_size += pieces[k];
-            }
-            if (pre_size <= P)
-                tableA[pre_size] += 1;
-
-            for(int j=1; j<M; j++) {
-                // j번째 조각부터 i개의 조각 이어붙이기
-                int end = (j + i - 1) % M;
-
-                int size = pre_size + pieces[end] - pieces[j-1];
-                pre_size = size;
-
-                // 계산된 크기에 해당하는 인덱스에 1 추가
-                if (size <= P) {
-                    tableA[size] += 1;
-                }
-
-            }
-        }
+        calcPizza(piecesA, tableA);
 
         // 피자 B 조각 입력받기
-        pieces = new int[N];
+        piecesB = new int[N];
         for(int i=0; i<N; i++) {
             piece = Integer.parseInt(reader.readLine());
-            pieces[i] = piece;
+            piecesB[i] = piece;
             if (piece <= P)
                 tableB[piece] += 1;
         }
 
         // 모든 조각을 이어붙이는 경우 계산
-        tmp = 0;
-        for(int k=0; k<N; k++) {
-            tmp += pieces[k];
-        }
-        if (tmp <= P)
-            tableB[tmp] = 1;
-
-        // 피자 개수별로 나올 수 있는 조합 계산
-        for(int i=2; i<N; i++) {
-            int pre_size = 0;
-
-            // 첫 번째 조각부터 i개의 조각 이어붙이기
-            for(int k=0; k<i; k++) {
-                pre_size += pieces[k];
-            }
-            if (pre_size <= P)
-                tableB[pre_size]++;
-
-            for(int j=1; j<N; j++) {
-                // j번째 조각부터 i개의 조각 이어붙이기
-                int end = (j + i - 1) % N;
-                int size = pre_size + pieces[end] - pieces[j-1];
-                pre_size = size;
-                // 계산된 크기에 해당하는 인덱스에 1 설정
-                if (size <= P) {
-                    tableB[size] += 1;
-                }
-            }
-        }
+        calcPizza(piecesB, tableB);
 
         long result = 0;
         for(int i=0; i<=P; i++) {
@@ -123,5 +64,43 @@ public class Main {
 
         System.out.println(result);
     }
+
+    public static void calcPizza(int[] pieces, int[] table) {
+        int length = pieces.length;
+
+        // 모든 피자 조각을 사용하는 경우 계산
+        int tmp = 0;
+        for(int k=0; k<length; k++) {
+            tmp += pieces[k];
+        }
+        if (tmp <= P)
+            table[tmp] = 1;
+
+        for(int i=2; i<length; i++) {
+            int pre_size = 0;
+
+            // 첫번째 조각부터 i개의 조각 이어붙이기
+            for(int k=0; k<i; k++) {
+                pre_size += pieces[k];
+            }
+            if (pre_size <= P)
+                table[pre_size] += 1;
+
+            for(int start=1; start<length; start++) {
+                // j번째 조각부터 i개의 조각 이어붙이기
+                int end = (start + i - 1) % length; // 이어붙일 마지막 조각의 인덱스
+                int size = pre_size + pieces[end] - pieces[start-1]; // 이전 사이드에서 마지막 인덱스 조각을 더하고, 시작 인덱스의 이전 조각을 뺀다.
+                pre_size = size; // 이전 사이드 업데이트
+                // 계산된 크기에 해당하는 인덱스에 1 추가
+                if (size <= P) {
+                    table[size] += 1;
+                }
+
+            }
+
+        }
+
+    }
+    
 
 }
