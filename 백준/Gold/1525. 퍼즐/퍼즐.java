7db02c3bc4d5e86d8bf348429ab5487ebc3bdac0
Hwang Jehyun <hyun353500@naver.com>
2025-03-14 01:08:52 +0900

    [Gold II] Title: 퍼즐, Time: 652 ms, Memory: 84020 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -2,17 +2,16 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-// 최적화 시도
+// 최적화 2
 public class Main {
 
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static int[][] puzzle;
-    public static int r, c;
+    public static int idx;
     public static Set<String> codeSet = new HashSet<>();
 
-    public static int[] dr = {-1, 1, 0, 0};
-    public static int[] dc = {0, 0, -1, 1};
+    public static int[] dx = {-3, 3, -1, 1};
 
     public static void init() throws Exception {
         puzzle = new int[3][3];
@@ -21,10 +20,8 @@ public class Main {
             tkn = new StringTokenizer(reader.readLine());
             for(int j=0; j<3; j++) {
                 puzzle[i][j] = Integer.parseInt(tkn.nextToken());
-                if (puzzle[i][j] == 0) {
-                    r = i;
-                    c = j;
-                }
+                if (puzzle[i][j] == 0)
+                    idx = i*3 + j;
             }
         }
 
@@ -33,12 +30,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         init();
         String code = makeCode(puzzle);
-        int result = bfs(r, c, code);
+        int result = bfs(idx, code);
         System.out.println(result);
     }
 
     // 재귀를 써야하나?
-    public static int bfs(int row, int col, String code) {
+    public static int bfs(int idx, String code) {
         int depth = 0;
 
         if (isCorrect(code))
@@ -48,30 +45,30 @@ public class Main {
         depth++;
         // 상하좌우 이동 (초기 큐 데이터 추가)
         for (int k=0; k<4; k++) {
-            int nr = row + dr[k];
-            int nc = col + dc[k];
+            int next_idx = idx + dx[k];
+
+            if (idx % 3 == 2 && (next_idx - idx) == 1) continue;
+            if (idx % 3 == 0 && (idx - next_idx) == 1) continue;
 
             // 배열 범위를 벗어나지 않고, 방문한 적 없는 칸인 경우 칸 이동
-            if (nr >=0 && nc >= 0 && nr < 3 && nc < 3) {
+            if (next_idx >= 0 && next_idx < 9) {
 
-                // 코드를 2차원 배열 형태의 퍼즐로 파싱
-                int[][] copy = parseCode(code);
+                // 칸 이동 (코드 내에서 문자열 인덱스 변경)
+                char[] seq = code.toCharArray();
+                seq[idx] = seq[next_idx];
+                seq[next_idx] = '0';
 
-                // 자리 바꾸기 (한쪽 값이 무조건 0인 swap 로직)
-                copy[row][col] = copy[nr][nc]; // 원래 0이 있던 자리는 올길 칸의 값으로 변경
-                copy[nr][nc] = 0; // 옮길 칸은 0으로 변경
-
-                // 현재 퍼즐의 상태를 코드로 변경
-                String update_code = makeCode(copy);
+                // 위치가 바뀐 코드를 새로운 String으로 생성
+                String update_code = new String(seq);
 
                 // 옮긴 상태가 조건을 만족하면 depth를 리턴하고 종료
                 if (isCorrect(update_code))
                     return depth;
                 // 만족하지 않을 경우 현재 상태를 code로 만들어 hashSet에 저장
                 else {
-                    Puzzle p = new Puzzle(nr, nc, update_code);
+                    Puzzle p = new Puzzle(next_idx, update_code);
                     q.add(p);
-                    codeSet.add(makeCode(copy));
+                    codeSet.add(update_code);
                 }
             }
 
@@ -89,30 +86,36 @@ public class Main {
 
                 // 상하좌우 탐색
                 for (int k=0; k<4; k++) {
-                    int nr = p.row + dr[k];
-                    int nc = p.col + dc[k];
+                    int crnt_idx = p.idx;
+                    String crnt_code = p.code;
+
+                    int next_idx = p.idx + dx[k];
+                    int nr = next_idx / 3;
+                    int nc = next_idx % 3;
+
+                    if (crnt_idx % 3 == 2 && (next_idx - crnt_idx) == 1) continue;
+                    if (crnt_idx % 3 == 0 && (crnt_idx - next_idx) == 1) continue;
 
                     // 배열 범위를 벗어나지 않고, 방문한 적 없는 칸인 경우 칸 이동
-                    if (nr >=0 && nc >= 0 && nr < 3 && nc < 3) {
+                    if (nr >= 0 && nc >= 0 && nr < 3) {
 
-                        // 현재 Puzzle의 코드를 파싱하여 2차원 배열로 생성
-                        int[][] copy = parseCode(p.code);
+                        // 칸 이동 (코드 내에서 문자열 인덱스 변경)
+                        char[] seq = crnt_code.toCharArray();
+                        seq[crnt_idx] = seq[next_idx];
+                        seq[next_idx] = '0';
 
-                        // 자리 바꾸기 (한쪽 값이 무조건 0인 swap 로직)
-                        copy[p.row][p.col] = copy[nr][nc]; // 원래 0이 있던 자리는 올길 칸의 값으로 변경
-                        copy[nr][nc] = 0; // 옮길 칸은 0으로 변경
-
-                        String updated_code = makeCode(copy);
+                        // 위치가 바뀐 코드를 새로운 String으로 생성
+                        String update_code = new String(seq);
 
                         // 옮긴 상태가 조건을 만족하면 depth를 리턴하고 종료
-                        if (isCorrect(updated_code))
+                        if (isCorrect(update_code))
                             return depth;
                             // 만족하지 않을 경우 현재 상태를 Puzzle 객체로 만들어 큐에 저장
                         else {
-                            if (!codeSet.contains(updated_code)) {
-                                Puzzle new_p = new Puzzle(nr, nc, updated_code);
+                            if (!codeSet.contains(update_code)) {
+                                Puzzle new_p = new Puzzle(next_idx, update_code);
                                 q.add(new_p);
-                                codeSet.add(updated_code);
+                                codeSet.add(update_code);
                             }
                         }
                     }
@@ -139,31 +142,18 @@ public class Main {
         return code.equals("123456780");
     }
 
-    public static int[][] parseCode(String code) {
-        char[] seq = code.toCharArray();
-
-        int[][] puzzle = new int[3][3];
-        for(int i=0; i<3; i++)
-            for(int j=0; j<3; j++)
-                puzzle[i][j] = seq[i*3 + j] - '0';
-
-        return puzzle;
-    }
-
     static class Puzzle {
-        public int row;
-        public int col;
+        public int idx;
         public String code;
 
-        public Puzzle(int row, int col, String code) {
-            this.row = row;
-            this.col = col;
+        public Puzzle(int idx, String code) {
+            this.idx = idx;
             this.code = code;
         }
 
         @Override
         public String toString() {
-            return String.format("row = %d, col = %d, code = %s\n", row, col, code);
+            return String.format("idx = %d, code = %s\n", idx, code);
         }
 
     }
