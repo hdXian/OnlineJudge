e30ca8269f2ca751e639c00141a3c25603c13c2a
Hwang Jehyun <hyun353500@naver.com>
2025-04-09 21:56:23 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 4.18 MB, 시간: 0.21 ms
+메모리: 4.22 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 09일 21:53:24
+2025년 04월 09일 21:56:18
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,54 +1,38 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_map>
 
 using namespace std;
-
-bool compare(pair<int, int> p1, pair<int, int> p2) {
-    if (p1.second == p2.second)
-        return p1.first < p2.first;
-    
-    return p1.second > p2.second;
-}
 
 vector<int> solution(vector<int> answers) {
     // 1번: 1, 2, 3, 4, 5, ...
     // 2번: 2, 1, 2, 3, 2, 4, 2, 5, ...
     // 3번: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
     
-    vector<int> pattern1 = {1, 2, 3, 4, 5}; // 6
+    vector<int> pattern1 = {1, 2, 3, 4, 5}; // 5
     vector<int> pattern2 = {2, 1, 2, 3, 2, 4, 2, 5}; // 8
     vector<int> pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
     
     int length = answers.size();
-    unordered_map<int, int> mm;
-    mm[1] = 0;
-    mm[2] = 0;
-    mm[3] = 0;
+    vector<int> students(3, 0);
     
     int val;
     for(int i=0; i<length; i++) {
         val = answers[i];
         
-        if (val == pattern1[i%5]) mm[1]++;
-        if (val == pattern2[i%8]) mm[2]++;
-        if (val == pattern3[i%10]) mm[3]++;
+        if (val == pattern1[i%5]) students[0]++;
+        if (val == pattern2[i%8]) students[1]++;
+        if (val == pattern3[i%10]) students[2]++;
     }
     
-    cout << mm[1] << endl;
-    cout << mm[2] << endl;
-    cout << mm[3] << endl;
-    
     int max_val = -1;
-    for(int i=1; i<=3; i++)
-        if (max_val < mm[i])
-            max_val = mm[i];
+    for(int i=0; i<3; i++)
+        if (max_val < students[i])
+            max_val = students[i];
     
     vector<int> answer;
-    for(int i=1; i<=3; i++) {
-        if (mm[i] == max_val)
-            answer.push_back(i);
+    for(int i=0; i<3; i++) {
+        if (students[i] == max_val)
+            answer.push_back(i+1);
     }
     
     return answer;
