87feca46e149180d313d51fba0d640868dc05a62
Hwang Jehyun <hyun353500@naver.com>
2025-04-12 18:46:28 +0900

    [level 1] Title: 실패율, Time: 7.85 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -4,65 +4,40 @@
 
 using namespace std;
 
-class Stage {
-    
-public:
-    int number;
-    double fail;
-    
-    Stage() {}
-    Stage(int number, double fail) {
-        this->number = number;
-        this->fail = fail;
-    }
-    void setNumber(int number) { this->number = number; }
-    void setFail(double fail) { this->fail = fail; }
-};
-
-bool compare(const Stage& s1, const Stage& s2) {
-    if (s1.fail == s2.fail)
-        return s1.number < s2.number;
-    
-    return s1.fail > s2.fail;
+bool compare(pair<double, int> p1, pair<double, int> p2) {
+    if (p1.first == p2.first)
+        return p1.second < p2.second;
+    return p1.first > p2.first;
 }
 
 vector<int> solution(int N, vector<int> stages) {
-    // 각 스테이지에 도달한 사람의 수를 센다.
-    // 각 스테이지를 클리어하지 못한 사람의 수를 센다. (아직 도달하지 못한 사람은 세지 않아야 함.)
-    // 실패율을 계산하고, 내림차순 정렬한다.
     
-    vector<int> reached;
-    reached.assign(N+1, 0); // 인덱스 1부터 쓰기
-    
-    vector<int> fails;
-    fails.assign(N+1, 0);
+    // 각 stage별로 도달한 사람과 실패한 사람을 구한다.
+    vector<int> reached(N+1);
+    vector<int> fails(N+1);
     
     for(int s: stages) {
-        for(int i=1; i<s; i++) {
+        for(int i=0; i<s; i++)
             reached[i]++;
-        }
-        if (s != N+1) {
-            reached[s]++;
-            fails[s]++;
-        }
+        fails[s-1]++;
     }
     
-    vector<Stage> ss;
-    double fail;
-    for(int i=1; i<=N; i++) {
+    // fails가 N번 인덱스에 해당하는건 끝까지 다 깬 사람. 실패율 계산할 때 고려 안할꺼임.
+    
+    vector<pair<double, int>> rates(N);
+    for(int i=0; i<N; i++) {
         if (reached[i] == 0)
-            fail = 0;
+            rates[i].first = 0;
         else
-            fail = (double)fails[i] / reached[i];
-        ss.push_back(Stage(i, fail));
+            rates[i].first = (double)fails[i] / reached[i];
+        rates[i].second = i+1;
     }
     
-    sort(ss.begin(), ss.end(), compare);
+    sort(rates.begin(), rates.end(), compare);
     
     vector<int> answer;
-    for(Stage s: ss) {
-        answer.push_back(s.number);
+    for(pair<double, int> r: rates) {
+        answer.push_back(r.second);
     }
-    
     return answer;
 }
