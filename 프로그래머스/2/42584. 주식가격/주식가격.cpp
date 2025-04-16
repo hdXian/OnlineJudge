6e9e450051160d4733d0720d46b601795bf8fe3e
Hwang Jehyun <hyun353500@naver.com>
2025-04-17 01:34:23 +0900

    [level 2] Title: 주식가격, Time: 26.17 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -8,21 +9,29 @@ vector<int> solution(vector<int> prices) {
     vector<int> answer(length, 0);
     
     // 모노 스택 알고리즘
-    vector<int> mono;
+    stack<int> mono;
     int tmp;
     for(int i=0; i<length; i++) {
         tmp = prices[i];
         
         // 1. 살아남은 항목들을 카운트한다.
-        for(int idx: mono) answer[idx]++;
+        // for(int idx: mono) answer[idx]++;
         
         // 2. 현재 주식 항목보다 가격이 높은 항목들을 스택에서 제거한다. (가격을 방어하지 못했다.)
-        while (!mono.empty() && prices[mono.back()] > tmp) {
-            mono.pop_back();
+        while (!mono.empty() && prices[mono.top()] > tmp) {
+            // mono.top()의 생존 기간 = (현재 시점 - mono.top()) (mono.top()은 해당 주식의 매수 시점)
+            answer[mono.top()] = i - mono.top();
+            mono.pop();
         }
         
         // 3. 현재 항목을 스택에 추가한다.
-        mono.push_back(i);
+        mono.push(i);
+    }
+    
+    // 마지막까지 스택에 남은 
+    while(!mono.empty()) {
+        answer[mono.top()] = (length-1) - mono.top();
+        mono.pop();
     }
     
     return answer;
