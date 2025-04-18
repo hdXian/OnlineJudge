e00129660821cce3cb5e19f948a5aad92708e579
Hwang Jehyun <hyun353500@naver.com>
2025-04-19 00:28:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -7,13 +7,13 @@ using namespace std;
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     
     queue<string> c1;
-    for(string s: cards1) c1.push(s);
+    for(const string& s: cards1) c1.push(s);
     queue<string> c2;
-    for(string s: cards2) c2.push(s);
+    for(const string& s: cards2) c2.push(s);
     
     string answer = "Yes";
     
-    for(string word: goal) {
+    for(const string& word: goal) {
         if (c1.front() == word) c1.pop();
         else if (c2.front() == word) c2.pop();
         else {
