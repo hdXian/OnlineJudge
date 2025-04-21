28d1a72538825d29de26cd01ef28d65873992fee
Hwang Jehyun <hyun353500@naver.com>
2025-04-22 00:18:02 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 41.19 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long[] numbers) {
+        
+        // 1. 주어진 수를 이진수로 변환한다.
+        int length = numbers.length;
+        String[] bins = new String[length];
+        for(int i=0; i<length; i++) {
+            bins[i] = Long.toString(numbers[i], 2);
+        }
+        
+        // 2. 포화 이진트리의 높이, 총 노드 개수를 구한다.
+        // 3. 부모가 0인 노드는 반드시 0이어야 한다.
+        int[] answer = new int[length];
+        for(int i=0; i<length; i++) {
+            answer[i] = calc(bins[i]) ? 1 : 0;
+        }
+        
+        return answer;
+    }
+    
+    // 2. 해당 이진수에 대해 포화 이진트리 표현 가능 여부 판단
+    static boolean calc(String bin) {
+        // 2-1. 트리의 높이와 노드 개수를 구한다.
+        int blength = bin.length();
+        int h = (int)Math.floor(Math.log(blength) / Math.log(2)) + 1;
+        int nodes = (int)Math.pow(2, h) - 1;
+        
+        // 2-2. 빈 노드 개수만큼 앞에 0을 붙여서 포화 이진트리로 만든 다음에, 이진탐색을 돌린다.
+        String zeros = "";
+        for(int i=0; i<nodes-blength; i++) zeros += "0";
+        
+        String fullBin = zeros + bin;
+        if (fullBin.charAt(fullBin.length() / 2) == '0') return false;
+        return binarySearch(fullBin, 1, 0, nodes-1);
+    }
+    
+    static boolean binarySearch(String fullBin, int parent, int start, int end) {
+        if (start == end) {
+            if ((parent == 0) && (fullBin.charAt(start) == '1')) return false;
+            else return true;
+        }
+        
+        int mid = (start + end) / 2;
+        
+        // 부모 노드가 0일 경우 -> 자식 노드도 모두 0이어야 함.
+        int cur = (fullBin.charAt(mid) == '1') ? 1 : 0;
+        if (parent == 0 && cur == 1) return false;
+        
+        boolean l_sub = binarySearch(fullBin, cur, start, mid-1);
+        boolean r_sub = binarySearch(fullBin, cur,  mid+1, end);
+        return l_sub && r_sub;
+    }
+    
+    
+}
