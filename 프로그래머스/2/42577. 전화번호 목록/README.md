89fb6fd3487e46354a8c2fcd6a7953545252bb60
Hwang Jehyun <hyun353500@naver.com>
2025-04-24 20:53:29 +0900

    [level 2] Title: 전화번호 목록, Time: 293.88 ms, Memory: 228 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 98.5 MB, 시간: 312.06 ms
+메모리: 228 MB, 시간: 293.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 20:35:45
+2025년 04월 24일 20:53:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,25 @@
 import java.util.*;
 
 class Solution {
+    
+    static Map<String, Integer> pfs = new HashMap<>();
+    
     public boolean solution(String[] phone_book) {
-        // 전화번호 목록을 문자열 사전 순으로 정렬한다.
-        // 어떤 번호가 다른 번호의 접두어라면, 정렬했을 때 바로 앞 순서에 위치할 것.
-        // ex) 119, 97674223, 1195524421 -> 119, 1195524421, 97674223
-        Arrays.sort(phone_book, Comparator.naturalOrder());
-        String cur, next;
-        for(int i=0; i<phone_book.length-1; i++) {
+        // 해시를 쓴다면
+        
+        // 각 전화번호를 전화번호-인덱스 형태로 Map에 저장한다.
+        int length = phone_book.length;
+        for(int i=0; i<length; i++) {
+            pfs.put(phone_book[i], i);
+        }
+        
+        // 각 전화번호에 대해 다른 전화번호의 접두어 여부를 확인한다.
+        String cur;
+        for(int i=0; i<length; i++) {
             cur = phone_book[i];
-            next = phone_book[i+1];
-            if (cur.length() > next.length()) continue; // 앞 문자열 길이가 더 길면 아래 코드 에러남
-            if (cur.equals(next.substring(0, cur.length()))) {
-                return false;
+            for(int j=0; j<cur.length(); j++) {
+                if (pfs.containsKey(cur.substring(0, j)))
+                    return false;
             }
         }
         
