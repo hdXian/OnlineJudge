afcfd932ea95be1b6f37ac88df33d8e80d39dfbc
Hwang Jehyun <hyun353500@naver.com>
2025-04-30 21:36:33 +0900

    [level 2] Title: 오픈채팅방, Time: 156.70 ms, Memory: 47.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=cpp) 
 
 ### 성능 요약
 
-메모리: 164 MB, 시간: 288.24 ms
+메모리: 47.5 MB, 시간: 156.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 22:51:50
+2025년 04월 30일 21:36:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 void process(unordered_map<string, string>& nicknames, vector<pair<string, bool>>& history, string msg) {
+    // msg: Enter uid1234 Muzi
     istringstream iss(msg);
     string str;
     
@@ -15,18 +16,22 @@ void process(unordered_map<string, string>& nicknames, vector<pair<string, bool>
     while (getline(iss, str, ' ')) {
         args.push_back(str);
     }
+    // args = ["Enter", "uid1234", "Muzi"]
+    // "Enter", "Leave", "Change"
+    // 'E', 'L', 'C'
     
     string uid, nickname;
-    char head = args[0][0];
+    // args[0] = "Enter" or "Laeve" or "Change"
+    char head = args[0][0]; // head = 'E', 'L', 'C'
     if (head == 'E') {
         uid = args[1];
         nickname = args[2];
-        nicknames[uid] = nickname;
-        history.push_back(make_pair(uid, true));
+        nicknames[uid] = nickname; // map에 저장
+        history.push_back(make_pair(uid, true)); // Enter uid1234 Muzi -> (uid1234, true)
     }
     else if (head == 'L') {
         uid = args[1];
-        history.push_back(make_pair(uid, false));
+        history.push_back(make_pair(uid, false)); // Leave uid1234 -> (uid1234, false)
     }
     else { // 'C'
         uid = args[1];
@@ -48,14 +53,17 @@ vector<string> solution(vector<string> record) {
     // 출력 메시지를 기록할 pair 리스트 (uid-in/out/change , 0/1/2)
     vector<pair<string, bool>> history;
     
+    // 1. record를 순회하면서 uid-닉네임을 매칭시킨다.
     for(string s: record) {
         process(nicknames, history, s);
     }
     
+    // pair<string, vool> p = (uid1234, true) ==> p.first = uid1234, p.second = true
+    // history = [ (uid1234, true), (uid1234, false), (uid4567, true) ]
     vector<string> answer;
     for(const pair<string, bool>& p: history) {
-        string line = nicknames[p.first] + "님이 ";
-        line += (p.second) ? "들어왔습니다." : "나갔습니다.";
+        string line = nicknames[p.first] + "님이 "; // line = "Prodo님이 "
+        line += (p.second) ? "들어왔습니다." : "나갔습니다."; // line = "Prodo님이 들어왔습니다."
         answer.push_back(line);
     }
         
