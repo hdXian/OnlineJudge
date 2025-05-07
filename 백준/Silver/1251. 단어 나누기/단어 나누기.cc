f7d5f3218020bfe8e9fe832f42c87980e4bf4b1f
Hwang Jehyun <hyun353500@naver.com>
2025-05-07 22:30:33 +0900

    [Silver V] Title: 단어 나누기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.cc
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.cc
@@ -10,6 +10,7 @@ void comb(string prefix, string postfix, int depth) {
     if (depth == 3) {
         postfix = string(postfix.rbegin(), postfix.rend());
         string res = prefix + postfix;
+
         result = min(result, res);
     }
     else {
@@ -17,7 +18,6 @@ void comb(string prefix, string postfix, int depth) {
         for(int i=0; i<length-3+depth; i++) {
             string cur = postfix.substr(0, i+1); // postfix에서 떼올 만큼만 자르기
             cur = string(cur.rbegin(), cur.rend()); // 뒤집기
-            // prefix + cur을 붙인 문자열을 prefix로, postfix의 남은 부분을 postfix로 지정해 다음 comb로 보내기
             comb(prefix + cur, postfix.substr(i+1), depth+1); 
         }
     }
