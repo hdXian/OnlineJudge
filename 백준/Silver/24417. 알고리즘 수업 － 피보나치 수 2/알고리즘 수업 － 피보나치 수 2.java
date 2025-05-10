d7097128af4b4c345e514f1b72e63ac6d724233c
Hwang Jehyun <hyun353500@naver.com>
2025-05-10 23:21:56 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 2556 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.java
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.java
@@ -5,24 +5,25 @@ import java.math.*;
 public class Main {
 
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    static int N;
+    static int N; // n의 피보나치 수
 
     static void calc() {
         // 재귀호출
         // (a + b) mod x = (a mod x + b mod x) mod x
         long p = 1000000007; // 10억 7
-        long pre1 = 1; // 1번째 전 실행 횟수
-        long pre2 = 1; // 2번째 전 실행 횟수
-        long recur_result = 0; // 재귀호출 실행횟수
+        // 재귀호출 실행횟수
+        long pre1 = 1;
+        long pre2 = 1;
+        long tmp;
         for(int i=0; i<N-2; i++) {
-            recur_result = (pre1 + pre2) % p;
-            pre2 = pre1;
-            pre1 = recur_result;
+            tmp = pre1;
+            pre1 = (pre1 + pre2) % p;
+            pre2 = tmp;
         }
 
         // dp 호출횟수
         long dp_result = N-2;
-        System.out.printf("%d %d\n", recur_result, dp_result);
+        System.out.printf("%d %d\n", pre1, dp_result);
     }
 
     public static void main(String[] args) throws Exception {
