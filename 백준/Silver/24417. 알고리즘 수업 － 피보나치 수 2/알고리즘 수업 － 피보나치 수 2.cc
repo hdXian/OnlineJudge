59a91fc01633c0774e65aaec2da3731e176005a1
Hwang Jehyun <hyun353500@naver.com>
2025-05-12 17:00:14 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 636 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+string calc(int n) {
+    // 재귀 호출 실행 횟수
+    // fibo(n)의 호출 횟수는 fibo(n-1) + fibo(n-2)
+    int prev1 = 1;
+    int prev2 = 1;
+    int p = 1000000007; // 10억 7
+
+    int recur_result = 0;
+    for(int i=0; i<n-2; i++) {
+        recur_result = (prev1 + prev2) % p;
+        prev2 = prev1;
+        prev1 = recur_result;
+    }
+
+    string result = to_string(recur_result) + " " + to_string(n-2);
+
+    return result;
+}
+
+int main() {
+    int N; // N의 피보나치 수. 5 ~ 2억
+    cin >> N;
+
+    string result = calc(N);
+    cout << result << endl;
+
+    return 0;
+}
