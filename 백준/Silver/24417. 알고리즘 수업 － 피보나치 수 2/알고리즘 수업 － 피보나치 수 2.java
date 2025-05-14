192ac5182e91da8d0cb28e0c6c3ce17689b2b794
Hwang Jehyun <hyun353500@naver.com>
2025-05-14 22:28:37 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 892 ms, Memory: 18584 KB -BaekjoonHub

--- 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.java
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.java
@@ -1,34 +1,23 @@
-import java.io.*;
 import java.util.*;
-import java.math.*;
 
 public class Main {
+    static final int MOD = 1000000007;
 
-    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    static int N; // n의 피보나치 수
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-    static void calc() {
-        // 재귀호출
-        // (a + b) mod x = (a mod x + b mod x) mod x
-        long p = 1000000007; // 10억 7
-        // 재귀호출 실행횟수
-        long pre1 = 1;
-        long pre2 = 1;
-        long tmp;
-        for(int i=0; i<N-2; i++) {
-            tmp = pre1;
-            pre1 = (pre1 + pre2) % p;
-            pre2 = tmp;
+        int n = sc.nextInt();
+
+        long a = 1;
+        long b = 1;
+        long result = 0;
+
+        for (int i = 3; i <= n; i++) {
+            result = (a + b) % MOD;
+            a = b;
+            b = result;
         }
 
-        // dp 호출횟수
-        long dp_result = N-2;
-        System.out.printf("%d %d\n", pre1, dp_result);
+        System.out.println(result + " " + (n - 2) % MOD);
     }
-
-    public static void main(String[] args) throws Exception {
-        N = Integer.parseInt(reader.readLine()); // n의 피보나치 수 (5 ~ 2억)
-        calc();
-    }
-
 }
