4c6a54fbf47d936d89eb7388b9b7a76b250b6d37
Hwang Jehyun <hyun353500@naver.com>
2025-05-18 00:49:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -1,45 +1,14 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int find_parent(int n, vector<int>& parents) {
-    if (parents[n] != n) parents[n] = find_parent(parents[n], parents);
-    return parents[n];
-}
-
-void union_node(int n1, int n2, int& count, vector<int>& parents) {
-    int p1 = find_parent(n1, parents);
-    int p2 = find_parent(n2, parents);
-
-    if (p1 == p2) return;
-
-    // 더 작은 쪽이 부모
-    if (p1 < p2) {
-        parents[p2] = p1;
-        count++;
-    }
-    else {
-        parents[p1] = p2;
-        count++;
-    }
-}
-
 int calc(int n, int m) {
-    // union-find를 활용한 MST (최소 신장 트리)
-    vector<int> parents(n+1);
-    for(int i=1; i<=n; i++) parents[i] = i;
 
-    int a, b;
-    int count = 0;
-    int result = 0;
-    for (int i=0; i<m; i++) {
-        cin >> a >> b;
-        union_node(a, b, count, parents);
-        if (count == n-1) result = count;
-    }
+    string buf;
+    getline(cin, buf, '\n');
+    for(int i=0; i<m; i++) getline(cin, buf, '\n');
 
-    return result;
+    return n-1;
 }
 
 int main() {
@@ -48,8 +17,7 @@ int main() {
     cin >> T;
 
     int N, M;
-    char tmp;
-    string result;
+    string result = "";
     for(int i=0; i<T; i++) {
         cin >> N >> M;
         result += to_string(calc(N, M)) + '\n';
