339c9c4256b51dd964dd39f4fc5561cf008c122e
Hwang Jehyun <hyun353500@naver.com>
2025-05-20 22:18:21 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,90 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int find_parent(int n, vector<int>& parents) {
+    if (parents[n] != n) parents[n] = find_parent(parents[n], parents);
+    return parents[n];
+}
+
+bool union_node(int n1, int n2, vector<int>& ranks, vector<int>& parents) {
+    int p1 = find_parent(n1, parents);
+    int p2 = find_parent(n2, parents);
+    
+    if (p1 == p2) return false;
+    
+    int r1 = ranks[p1];
+    int r2 = ranks[p2];
+    if (r1 == r2) {
+        parents[p2] = p1;
+        ranks[p1]++;
+    }
+    else if (r1 > r2) {
+        parents[p2] = p1;
+    }
+    else
+        parents[p1] = p2;
+    
+    return true;
+}
+
+struct Edge {
+    int p1, p2;
+    int cost;
+    Edge(int p1, int p2, int cost) {
+        this->p1 = p1;
+        this->p2 = p2;
+        this->cost = cost;
+    }
+};
+
+struct EdgeComparator {
+    bool operator() (const Edge& e1, const Edge& e2) {
+        return -(e1.cost) < -(e2.cost); // cost 기준 오름차순 (최대 큐이므로 음수 처리하여 비교)
+    }
+};
+
+int solution(int n, vector<vector<int>> costs) {
+    // costs[i]: 섬1 섬2 비용
+    
+    // 1. 각 섬을 연결하는 도로 정보를 저장한다.
+    // 2. 각 도로를 비용 순으로 오름차순 정렬하고 하나씩 뽑는다.
+    // 3. 뽑은 도로가 연결한 양쪽 섬이 같은 집합에 포함되지 않은 경우, 해당 도로를 선택한다.
+    // 4. 같은 집합에 포함되어있는 경우, 어떻게든 연결되어 있다는 의미이므로 해당 도로를 선택하지 않는다.
+    // 5. 선택한 도로 수가 n-1개가 되거나 큐가 빌 때까지 반복한다.
+    // 6. 문제 조건에는 없지만, 큐가 빌 때까지 도로를 뽑았는데, 선택된 도로 수가 n-1보다 적은 경우,
+    // -> 모든 섬을 연결할 방법이 없는 케이스로 판별한다.
+    
+    // union-find에 사용할 부모, 랭크 배열 선언
+    vector<int> parents(n);
+    for(int i=0; i<n; i++) parents[i] = i;
+    
+    vector<int> ranks(n, 0);
+    
+    
+    // 비용 순으로 내림차순 정렬하는 pq 선언
+    priority_queue<Edge, vector<Edge>, EdgeComparator> pq;
+    int siz = costs.size();
+    int p1, p2, cost;
+    for(int i=0; i<siz; i++) {
+        p1 = costs[i][0];
+        p2 = costs[i][1];
+        cost = costs[i][2];
+        pq.push(Edge(p1, p2, cost));
+    }
+    
+    int edge_count = 0;
+    int total_cost = 0;
+    while(!pq.empty() && edge_count<n-1) {
+        Edge cur = pq.top();
+        if (union_node(cur.p1, cur.p2, ranks, parents)) {
+            edge_count++;
+            total_cost += cur.cost;
+        }
+        pq.pop();
+    }
+    
+    return total_cost;
+}
