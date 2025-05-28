6ab949f8aebf616815c38874f67e7d070643dfc5
Hwang Jehyun <hyun353500@naver.com>
2025-05-28 16:12:56 +0900

    [level 3] Title: 섬 연결하기, Time: 1.05 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
 
 ### 성능 요약
 
-메모리: 3.71 MB, 시간: 0.04 ms
+메모리: 91.1 MB, 시간: 1.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 20일 22:23:40
+2025년 05월 28일 16:12:53
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,82 +1,87 @@
-import java.io.*;
 import java.util.*;
-import java.math.*;
 
 class Solution {
-
-    static int[] parents;
-
+    
     static class Edge implements Comparable<Edge> {
-        public int p1;
-        public int p2;
-        public int cost;
-
+        int p1, p2;
+        int cost;
+        
         public Edge(int p1, int p2, int cost) {
             this.p1 = p1;
             this.p2 = p2;
             this.cost = cost;
         }
-
+        
         @Override
         public int compareTo(Edge e) {
             return Integer.compare(this.cost, e.cost);
         }
-
+        
     }
-
+    
+    static int[] parents;
+    static int[] ranks;
+    
     static int findParent(int n) {
         if (parents[n] != n) parents[n] = findParent(parents[n]);
         return parents[n];
     }
-
+    
     static boolean union(int n1, int n2) {
         int p1 = findParent(n1);
         int p2 = findParent(n2);
-
+        
         if (p1 == p2) return false;
         
-        if (p1 < p2) parents[p2] = p1;
+        int r1 = ranks[p1];
+        int r2 = ranks[p2];
+        if (r1 == r2) {
+            parents[p2] = p1;
+            ranks[p1]++;
+        }
+        else if (r1 > r2) {
+            parents[p2] = p1;
+        }
         else parents[p1] = p2;
-
+        
         return true;
     }
-
+    
     public int solution(int n, int[][] costs) {
-        // 섬 개수 n: 1 ~ 100
-        // costs[i]: 번호1, 번호2, 비용]
-        // uinon-find 기반 MST.
-
+        // n: 섬 개수. 1 ~ 100
+        // costs: 다리들
+        // costs[i][0]: 번호1, costs[i][1]: 번호2, costs[i][2]: 비용
+        // 양방향 간선. 같은 간선 주어지지 않음.
+        
+        // MST 만들기
+        // 1. 부모 배열과 랭크 배열을 초기화한다.
         parents = new int[n];
+        ranks = new int[n];
         for(int i=0; i<n; i++) {
             parents[i] = i;
+            ranks[i] = 0;
         }
-
-        // 1. 도로 정보를 받아 Edge 클래스로 저장한다.
-        // 2. Edge들을 비용이 적은 순으로 정렬하고, union-find를 이용하여 모든 섬이 포함될 때까지 Edge를 선택한다.
-        // 3. 모든 Edge가 선택될 때까지 합한 간선 비용을 리턴한다.
+        
+        // 2. 간선 정보를 받아 비용 순으로 오름차순으로 정렬한다.
+        int siz = costs.length;
         PriorityQueue<Edge> pq = new PriorityQueue<>();
-        int p1, p2, cost;
-        for(int i=0; i<costs.length; i++) {
-            p1 = costs[i][0];
-            p2 = costs[i][1];
-            cost = costs[i][2];
-            pq.add(new Edge(p1, p2, cost));
+        for(int i=0; i<siz; i++) {
+            // 점1, 점2, 비용
+            pq.add(new Edge(costs[i][0], costs[i][1], costs[i][2]));
         }
-
+        
+        // 3. 큐가 비거나 n-1개의 간선이 선택될 때까지 서로 다른 집합에 속한 간선을 선택한다.
         int edge_count = 0;
         int total_cost = 0;
         while(!pq.isEmpty() && edge_count < (n-1)) {
-            Edge e = pq.poll();
-            if (union(e.p1, e.p2)) {
-                // System.out.printf("%d과 %d 사이의 간선이 추가됨\n", e.p1, e.p2);
+            Edge cur = pq.poll();
+            if (union(cur.p1, cur.p2)) {
+                total_cost += cur.cost;
                 edge_count++;
-                total_cost += e.cost;
             }
         }
-
+        
         return total_cost;
-
-
     }
 
 }
