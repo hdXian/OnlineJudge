2f8d0b77cc0d8db42ae5216f3216dff27e15a4cd
Hwang Jehyun <hyun353500@naver.com>
2025-06-10 01:57:35 +0900

    [Gold III] Title: 체스판 위의 공, Time: 604 ms, Memory: 57116 KB -BaekjoonHub

--- 백준/Gold/16957. 체스판 위의 공/체스판 위의 공.java
+++ 백준/Gold/16957. 체스판 위의 공/체스판 위의 공.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.math.*;
 
 public class Main {
 
@@ -13,7 +12,16 @@ public class Main {
     static int R, C;
 
     static int[][] board;
-    static int[] parents;
+    static int[][] counts;
+    static Point[][] table;
+
+    static class Point {
+        int row, col;
+        public Point(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
 
     static void init() throws Exception {
         StringTokenizer tkn = new StringTokenizer(reader.readLine());
@@ -21,83 +29,71 @@ public class Main {
         C = Integer.parseInt(tkn.nextToken());
 
         board = new int[R][C];
+        counts = new int[R][C];
+        table = new Point[R][C];
+
         for(int i=0; i<R; i++) {
             tkn = new StringTokenizer(reader.readLine());
             for(int j=0; j<C; j++) {
                 board[i][j] = Integer.parseInt(tkn.nextToken());
+                counts[i][j] = 0;
+                table[i][j] = new Point(-1, -1);
             }
         }
 
-        parents = new int[R*C];
-        for(int i=0; i<R*C; i++) {
-            parents[i] = i;
-        }
-
     }
 
-    static int findParent(int n) {
-        if (parents[n] != n) parents[n] = findParent(parents[n]);
-        return parents[n];
-    }
+    static Point dfs(Point cur) {
+        int r = cur.row;
+        int c = cur.col;
+        if (table[r][c].row != -1) return table[r][c];
 
-    static void union(int n1, int n2) {
-        int p1 = findParent(n1);
-        int p2 = findParent(n2);
-
-        int r1 = p1 / C; // 행 번호는 번호를 (열 길이)로 나눈 몫.
-        int c1 = p1 % C; // 열 번호는 번호를 (열 길이)로 나눈 나머지.
-
-        int r2 = p2 / C;
-        int c2 = p2 % C;
-
-        if (board[r1][c1] < board[r2][c2]) parents[p2] = p1;
-        else parents[p1] = p2;
-    }
-
-    static void search(int r, int c) {
-        int cur = r * C + c;
-
-        int min_num = board[r][c];
         int min_r = r;
         int min_c = c;
 
+        // 주변에서 가장 숫자가 작은 좌표를 탐색
         for(int i=0; i<8; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
             if (nr >=0 && nc >=0 && nr < R && nc < C) {
-                if (board[nr][nc] < min_num) {
-                    min_num = board[nr][nc];
+                if (board[nr][nc] < board[min_r][min_c]) {
                     min_r = nr;
                     min_c = nc;
                 }
             }
         }
 
-        int next = min_r * C + min_c;
-        union(cur, next);
+        // 주변에 자신보다 작은 값이 존재하지 않으면 자기 자신을 테이블에 저장
+        // 재귀호출로 인해 최종적으로 이 조건에 도달하면, 해당 Point 값을 저장하고 리턴함으로써 재귀호출 경로 상의 모든 함수에서 이 경로를 테이블에 저장하게 됨.
+        if (min_r == r && min_c == c) {
+            table[cur.row][cur.col] = new Point(cur.row, cur.col);
+        }
+        // 존재하면 dfs 다시 호출
+        else {
+            table[cur.row][cur.col] = dfs(new Point(min_r, min_c));
+        }
+
+        return table[cur.row][cur.col];
     }
 
     static void calc() {
         // 모든 요소들에 대해 탐색을 돌린다.
         for(int i=0; i<R; i++) {
             for(int j=0; j<C; j++) {
-                search(i, j);
+                Point result = dfs(new Point(i, j));
+                counts[result.row][result.col]++;
             }
         }
 
-        int[] counts = new int[R*C];
-        for(int i=0; i<R*C; i++) {
-            int p = findParent(i);
-            counts[p]++;
-        }
-
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<R; i++) {
             for(int j=0; j<C; j++) {
-                System.out.print(counts[i*C + j] + " ");
+                sb.append(counts[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append('\n');
         }
 
+        System.out.println(sb);
     }
 
     public static void main(String[] args) throws Exception {
