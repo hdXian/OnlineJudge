646f65ed14ca38dd0f40d3df5d5a0a64e58dd40a
Hwang Jehyun <hyun353500@naver.com>
2025-06-21 23:27:31 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,51 @@
-import java.util.*;
-
 class Solution {
     
-    static int N;
-    static boolean[] visited;
+    static int[] parents;
+    static int[] ranks;
     
-    static void dfs(int node, int[][] computers) {
-        visited[node] = true;
+    static int findParent(int n) {
+        if (parents[n] != n) parents[n] = findParent(parents[n]);
+        return parents[n];
+    }
+    
+    static void union(int n1, int n2) {
+        int p1 = findParent(n1);
+        int p2 = findParent(n2);
         
-        for(int i=0; i<N; i++) {
-            if(!visited[i] && computers[node][i] == 1) {
-                dfs(i, computers);
-            }
+        if (p1 == p2) return;
+        
+        int r1 = ranks[p1];
+        int r2 = ranks[p2];
+        if (r1 == r2) {
+            parents[p2] = p1;
+            ranks[p1]++;
         }
-        
+        else if (r1 > r2) parents[p2] = p1;
+        else parents[p1] = p2;
     }
     
     public int solution(int n, int[][] computers) {
-        // 1. visited 배열을 선언한다.
-        N = n;
-        visited = new boolean[N];
-        Arrays.fill(visited, false);
+        // 1. parents, ranks 배열을 선언한다.
+        parents = new int[n];
+        ranks = new int[n];
+        for(int i=0; i<n; i++) {
+            parents[i] = i;
+            ranks[i] = 1;
+        }
         
-        // 2. dfs를 돌면서 카운트한다.
-        int count = 0;
-        for(int i=0; i<N; i++) {
-            if (!visited[i]) {
-                dfs(i, computers);
-                count++;
+        // 2. 연결되어 있는 두 노드를 union한다.
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if (computers[i][j] == 1) union(i, j);
             }
         }
         
-        return count;
+        // 3. root 노드의 개수를 센다.
+        int roots = 0;
+        for(int i=0; i<n; i++) {
+            if (findParent(i) == i) roots++;
+        }
+        
+        return roots;
     }
 }
