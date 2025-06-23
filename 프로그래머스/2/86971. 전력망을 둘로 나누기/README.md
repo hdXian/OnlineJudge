40a89c2acc3ecdac5727091b95c3a8c5a75bbf29
Hwang Jehyun <hyun353500@naver.com>
2025-06-23 21:25:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.11 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.38 ms
+메모리: 4.11 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 23일 21:02:36
+2025년 06월 23일 21:25:00
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,87 +1,55 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> parents;
-vector<int> ranks;
-
 int N;
-int result = 100;
+int result = 1000;
 
-int find_parent(int n) {
-    if (parents[n] != n) parents[n] = find_parent(parents[n]);
-    return parents[n];
-}
+vector<bool> visited;
+int tmp_count;
 
-void union_node(int n1, int n2) {
-    int p1 = find_parent(n1);
-    int p2 = find_parent(n2);
+vector<vector<int>> neighbors;
+
+void dfs(int n, vector<int>& pass_wire) {
+    visited[n] = true;
+    tmp_count++;
     
-    if (p1 == p2) return;
-    
-    int r1 = ranks[p1];
-    int r2 = ranks[p2];
-    
-    if (r1 == r2) {
-        parents[p2] = p1;
-        ranks[p1]++;
+    for(int neighbor: neighbors[n]) {
+        if (pass_wire[0] == n && pass_wire[1] == neighbor) continue;
+        if(!visited[neighbor]) dfs(neighbor, pass_wire);
     }
-    else if (r1 > r2) parents[p2] = p1;
-    else parents[p1] = p2;
     
 }
 
-void div(int pass_offset, vector<vector<int>>& wires) {
-    
-    parents = vector<int>(N+1);
-    ranks = vector<int>(N+1);
-    for(int i=1; i<=N; i++) {
-        parents[i] = i;
-        ranks[i] = 1;
-    }
-    
-    int n1, n2;
-    for(int i=0; i<N-1; i++) {
-        if (i == pass_offset) continue;
-        n1 = wires[i][0];
-        n2 = wires[i][1];
-        union_node(n1, n2);
-    }
-    
-    unordered_map<int, int> mm;
-    
-    int p;
-    for(int i=1; i<=N; i++) {
-        p = find_parent(i);
-        mm[p]++;
-    }
-    
-    bool flag = true;
-    int sum = 0;
-    for(auto it=mm.begin(); it != mm.end(); it++) {
-        if(flag) {
-            sum += it->second;
-            flag = false;
-        }
-        else sum = abs(sum - it->second);
-    }
-    
-    // cout << "sum: " << sum << endl;
-    result = min(result, sum);
+void div(vector<int>& pass_wire) {
+    visited = vector<bool>(N+1, false);
+    tmp_count = 0;
+    int r = pass_wire[0]; // 끊어진 간선 둘 중 하나를 루트로 잡아서 dfs를 돌려본다.
+    dfs(r, pass_wire);
+    result = min(result, abs(tmp_count - (N-tmp_count)));
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    // wires 길이: n-1.
     N = n;
     
-    // 1. 각 전선을 제외하고 union-find로 집합을 형성한다.
-    // 2. 형성된 각 집합의 원소 개수 차이 절댓값을 계산한다.
-    // 3. 차이가 가장 적은 경우를 저장해 리턴한다.
-    for(int i=0; i<N-1; i++) {
-        div(i, wires);
+    // 인접 리스트 초기화
+    neighbors = vector<vector<int>>(N+1);
+    int n1, n2;
+    for(const auto& wire: wires) {
+        n1 = wire[0];
+        n2 = wire[1];
+        neighbors[n1].push_back(n2);
+        neighbors[n2].push_back(n1);
+    }
+    
+    // 1. 각 전력망을 하나씩 제거한 다음 루트노드부터 dfs를 돌려본다.
+    // 2. (전체 노드 개수) - (dfs로 구한 노드 개수) = 나머지 한쪽 전력망의 노드 개수
+    // 3. 둘의 차이가 가장 적은 경우를 저장하고 리턴한다.
+    for(vector<int>& wire: wires) {
+        div(wire);
     }
     
     return result;
